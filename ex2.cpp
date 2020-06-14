70c6e5a6aa1eb1e6b1577d4df897c61aa9e218f6
Aryan Irani <64119688+aryanirani123@users.noreply.github.com>
2020-06-14 14:30:22 +0530

    Initial commit

---
+++ ex2.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+Chapter2 - Input,Processing and Output
+Ex1 - Create a program that prompts for an input string and displays output that s
+hows the input string and the number of characters the string contains.
+Author: Aryan Irani(aryanirani123@gmail.com)
+*******************************************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main(){
+    
+    string input;
+    cout << "What is the input string? ";
+    cin >> input;
+
+    
+    int cnt = 0;
+    for(int i=0; input[i]; i++){
+        	cnt++;
+    }
+ 	
+    cout << cnt << endl;
+     return 0;
+}
