c0faa83b6b5b5331f83e4edc9ba69482e89f7043
Aryan Irani <64119688+aryanirani123@users.noreply.github.com>
2020-06-14 11:08:00 +0530

    Initial commit

---
+++ ex1.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+Chapter2 - Input,Processing and Output
+Ex1 - Saying hello
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "What is your name: ";
+    cin >> name;
+    
+    cout << "Hello," << name << " nice to meet you ";
+    
+}
