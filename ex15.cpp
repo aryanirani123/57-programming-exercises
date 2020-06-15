de98365f2fd9558c9fa03e98a3566b4912c02e0b
Aryan Irani <64119688+aryanirani123@users.noreply.github.com>
2020-06-15 12:34:32 +0530

    Update ex15.cpp

--- ex15.cpp
+++ ex15.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
-Chapter3 - Calculations 
+Chapter3 - Making Decisions  
 Ex15 - Create a simple program that validates user login credentials. The program must prompt the user for a username and password. 
 The program should compare the password given by the user to a known password. If the password matches, the program should display 
 “Welcome!” If it doesn’t match, the program should display “I don’t know you.”
